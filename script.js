9df2bf0bc5c18257f81964cc5cbd27555819c0d8
B.Dhansai <150512590+dhansai098@users.noreply.github.com>
2024-02-25 22:49:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("Hello world");
+
+// variables
+
+var1=5;
+var2=9;
+var3=12;
+console.log(var1);
+console.log(var2);
+console.log(var3);
+console.log("var1");
+
+
+demo=12;
+demo=24;
+console.log(demo);
+console.log("demo");
+
+//Data Types     Intergers  String  float  Boolean
+
+
+let number7=6;
+console.log(number7);// integer
+
+let number8=90.1;
+console.log(number8); // float
+
+let name="dhansai";
+console.log(name); //string
+
+let sample=true;
+console.log(sample); // boolean
+
+
+
+// comments
+
+/* this is comment line for a group of code
+1
+2
+3
+*/
